f6a9774133e9d4b03841aff9456bb2fe070dc7e3
Victor Sanchez <omarsanchez16@live.com>
2022-06-08 21:44:04 -0500

    Agregando el style switcher

---
+++ js/style-switcher.js
@@ -0,0 +1,27 @@
+/* =========================== toggle style switcher =========================== */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click",() => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+//hide style - switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+/* =========================== colores de tema =========================== */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function SetActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
