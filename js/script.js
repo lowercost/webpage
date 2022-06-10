ba91cfefbd114bb6f0c7fd01e3c6bca532b281d9
Victor Sanchez <omarsanchez16@live.com>
2022-06-10 17:13:58 -0500

    Agregando typing animación

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========== Typing animation ========== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Programmer"],
+    typeSpeed:120,
+    BackSpeed:10,
+    loop:true
+})
